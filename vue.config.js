f8f180d6da8a9cbe7dd7fa1893ccba75e23cc8cb
olioleola <alehsiarheyeu@gmail.com>
2025-01-21 14:18:06 +0100

    upload vue wrapper to npm; adjust config; update bundle;

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,14 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: false,
   configureWebpack: {
-    externals: ['react', 'react-dom', '@seatmaps.com/react-lib'],
+    externals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+      '@seatmaps.com/react-lib': {
+        commonjs: '@seatmaps.com/react-lib',
+        commonjs2: '@seatmaps.com/react-lib',
+        root: 'SeatmapsReactLib',
+      },
+    },
   },
 });
