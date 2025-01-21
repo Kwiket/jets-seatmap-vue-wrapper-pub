fc746729bb714b524e2ba37fe8d3e6949ab85490
olioleola <alehsiarheyeu@gmail.com>
2025-01-21 13:17:40 +0100

    upload vue wrapper to npm; adjust gitignore; add mocks;

---
+++ src/mock-data/availability_mock.js
@@ -0,0 +1,89 @@
+const AVAILABILITY_MOCK = [
+  {
+    currency: 'USD',
+    label: '8A',
+    price: 500,
+    onlyForPassengerType: ['ADT', 'CHD', 'INF'],
+    additionalProps: [
+      { label: 'Test prop for all', icon: null },
+      { label: 'Another test prop for all', icon: 'wifi' },
+    ],
+    color: 'green',
+  },
+  {
+    currency: 'USD',
+    label: '8B',
+    price: 300,
+    onlyForPassengerType: ['ADT', 'CHD', 'INF'],
+    additionalProps: [
+      { label: 'Test prop for all', icon: null },
+      { label: 'Another test prop for all', icon: 'wifi' },
+    ],
+    color: 'green',
+  },
+  {
+    currency: 'USD',
+    label: '8C',
+    price: 100,
+    onlyForPassengerType: ['ADT', 'CHD', 'INF'],
+    additionalProps: [
+      { label: 'Test prop for all', icon: null },
+      { label: 'Another test prop for all', icon: 'wifi' },
+    ],
+    color: 'green',
+  },
+  {
+    currency: 'USD',
+    label: '20A',
+    price: 33,
+    onlyForPassengerType: ['ADT', 'CHD', 'INF'],
+    additionalProps: [
+      { label: 'Test prop for all', icon: null },
+      { label: 'Another test prop for all', icon: 'wifi' },
+    ],
+    color: 'lightgrey',
+  },
+  {
+    currency: 'USD',
+    label: '20E',
+    price: 33,
+    onlyForPassengerType: ['ADT', 'CHD', 'INF'],
+    additionalProps: [
+      { label: 'Clear air', icon: null },
+      { label: 'USB plug', icon: 'power' },
+    ],
+    color: 'red',
+  },
+  {
+    currency: 'USD',
+    label: '20K',
+    price: 33,
+    onlyForPassengerType: ['ADT', 'CHD', 'INF'],
+    color: 'magenta',
+  },
+  {
+    currency: 'USD',
+    label: '21F',
+    price: 13,
+    onlyForPassengerType: ['ADT', 'CHD', 'INF'],
+  },
+  {
+    currency: 'USD',
+    label: '21J',
+    price: 13,
+    onlyForPassengerType: ['CHD', 'INF'],
+  },
+  {
+    currency: 'USD',
+    label: '35K',
+    price: 1337,
+    onlyForPassengerType: ['CHD', 'INF'],
+  },
+  {
+    currency: 'EUR',
+    label: '70E',
+    price: 1234,
+  },
+];
+
+export default AVAILABILITY_MOCK;
